27c937b6d31fbae5f1aee660d1bf5ffa6c57d433
Lenny <lenny@itslennysfault.com>
2015-04-07 14:16:25 -0700

    cleanups

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,29 @@
+//when DOM is ready
 $(function(){
   // alert('loaded');
 
+  //listen for form submit
   $('#plural-form').on('submit',function(e){
     e.preventDefault();
 
+    //validate form field
     if($('#word-text').val().length < 1 || $('#word-text').val().split(' ').length > 1){
       alert('Enter exactly ONE word.');
       return;
     }
 
+    //make ajax call (get plural)
     $.ajax({
       url:$(this).attr('action'),
       data:$(this).serialize(),
       method:'GET'
     }).done(function(data){
-      console.log('yay',data);
+      // console.log('yay',data);
+      //update view (display result from server)
       $('#plural-result').html('One <strong>'+data.word+'</strong> or many <strong>'+data.plural+'</strong>');
-    })
+    });
 
-  })
-})
+  });// end on submit listener
+
+
+}); //end on DOM ready
